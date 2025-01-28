ac1124bd61a5762cd0fefb2f83a93473bbe2966a
Mykhailo <nightmisha8@gmail.com>
2025-01-28 22:12:33 +0200

    bash, zsh: added slink function

--- .config/zsh/zshrc.d/zsh_functions.sh
+++ .config/zsh/zshrc.d/zsh_functions.sh
@@ -1,17 +1,29 @@
 # create tmp dir and cd to it
 function cdtmp() {
-	cd $(mktemp -d)
+  cd $(mktemp -d)
 }
 
 function edit() {
-	# Edit a file with the default editor
-	local file=$(find . -type f | fzf) && ${EDITOR:-nvim} "$file"
+  # Edit a file with the default editor
+  local file=$(find . -type f | fzf) && ${EDITOR:-nvim} "$file"
 }
 
 function kproc() {
-	# Kill a process by name
-	local pid=$(ps -ef | sed 1d | fzf | awk '{print $2}')
-	if [ -n "$pid" ]; then
-		kill -9 "$pid"
-	fi
+  # Kill a process by name
+  local pid=$(ps -ef | sed 1d | fzf | awk '{print $2}')
+  if [ -n "$pid" ]; then
+    kill -9 "$pid"
+  fi
+}
+
+# function to create symbolic link from source to target
+function slink() {
+  local sorce=$1
+  local target=$2
+
+  if [[ -z "$sorce" || -z "$target" ]]; then
+    echo "usage: slink <sourse> <target>"
+  else
+    ln -sv "$sorce" "$target"
+  fi
 }
